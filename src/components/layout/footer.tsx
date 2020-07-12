9c91af8ccbee010f664c6f342c68cd352d6a0dd7
jean sidharta <jeansidharta@gmail.com>
2020-07-12 08:43:20 -0300

    Added volume slider

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,22 +6,30 @@ import { usePlayingMusic } from '../../contexts/playing-music';
 
 const Root = styled.footer`
 	border-top: 1px solid rgba(0, 0, 0, 0.3);
-	height: 10vh;
+	height: 15vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
 `;
 
 const ControlsContainer = styled.div`
 	display: flex;
-	width: 100%;
-	height: 100%;
 	justify-content: center;
 	align-items: center;
 `;
 
 const ActionButtonStyles = css`
-	height: 32px;
-	width: 32px;
+	height: 24px;
+	width: 24px;
 	cursor: pointer;
 	margin: 0 8px;
+`;
+
+const VolumeSlider = styled.input.attrs(props => ({ type: 'range', ...props }))`
+	margin-top: 12px;
+	max-width: 300px;
+	width: 100%;
 `;
 
 const PlayButton = styled(Images.Icons.PlayButton).attrs(() => ({ css: ActionButtonStyles}))``;
@@ -32,7 +40,23 @@ const DoublePrevButton = styled(Images.Icons.DoublePrevButton).attrs(() => ({ cs
 const DoubleNextButton = styled(Images.Icons.DoubleNextButton).attrs(() => ({ css: ActionButtonStyles}))``;
 
 const Footer = () => {
-	const { pause, resume, musicStatus: { playing } } = usePlayingMusic();
+	const volumeSliderRef = React.useRef<HTMLInputElement>(null);
+	const {
+		pause,
+		resume,
+		setVolume,
+		musicStatus: { playing, volume }
+	} = usePlayingMusic();
+
+	function updateVolume (event: React.ChangeEvent<HTMLInputElement>) {
+		const value = event.target.value;
+		setVolume(Number(value));
+	}
+
+	React.useEffect(() => {
+		if (!volumeSliderRef.current) return;
+		volumeSliderRef.current.value = volume.toString();
+	}, [volume]);
 
 	return (
 		<Root>
@@ -46,6 +70,13 @@ const Footer = () => {
 				<NextButton />
 				<DoubleNextButton />
 			</ControlsContainer>
+			<VolumeSlider
+				min="0"
+				max="1"
+				step="0.01"
+				onChange={updateVolume}
+				ref={volumeSliderRef}
+			/>
 		</Root>
 	);
 }
