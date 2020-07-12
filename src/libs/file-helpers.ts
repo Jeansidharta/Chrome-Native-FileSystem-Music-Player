b3ccd80c511a42d1b7057b03f23d387b788e30a8
jean sidharta <jeansidharta@gmail.com>
2020-07-12 10:05:53 -0300

    Added error message to user without the Native File System API

--- src/libs/file-helpers.ts
+++ src/libs/file-helpers.ts
@@ -1,4 +1,13 @@
+import { toast } from "react-toastify";
+
 export async function openDirectory () {
-	const handle = await window.chooseFileSystemEntries({ type: 'open-directory' });
+	let promise;
+	try {
+		promise = window.chooseFileSystemEntries({ type: 'open-directory' })
+	} catch (e) {
+		toast.error('Whoops! Seems like I can\'t access the Native File System API. Make sure you\'re using Chrome and have enabled the "Native File System API" flag on chrome://flags');
+		throw e;
+	}
+	const handle = await promise;
 	return handle;
 }
