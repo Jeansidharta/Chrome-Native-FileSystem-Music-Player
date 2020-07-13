707170d76cccc3dff5b8f6acc6c0f7d8823f2183
jean sidharta <jeansidharta@gmail.com>
2020-07-13 15:30:05 -0300

    searchbar works in home page

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -4,6 +4,7 @@ import { useFileSystem } from '../../contexts/file-system';
 import MusicItem from './music-item';
 import styled from 'styled-components';
 import { usePlayingMusic } from '../../contexts/playing-music';
+import { useSortFilter } from '../../contexts/sort-filter';
 
 const MainRoot = styled.main`
 	padding: 32px;
@@ -12,15 +13,22 @@ const MainRoot = styled.main`
 function Home () {
 	const { fileSystem } = useFileSystem();
 	const { setQueue } = usePlayingMusic();
+	const { filterFunction, sortFunction, searchString } = useSortFilter();
+
+	function searchStringFilter (a: FileSystemFileHandle) {
+		return a.name.trim().toLowerCase().includes(searchString.trim().toLowerCase());
+	}
+
+	const cleanMusicList = fileSystem?.music.filter(searchStringFilter);
 
 	function handleMusicClick (musicIndex: number) {
-		setQueue(fileSystem!.music.slice(musicIndex), true);
+		setQueue(cleanMusicList!.slice(musicIndex), true);
 	}
 
 	function renderMusicItems () {
-		if (!fileSystem) return <>Please, open a music folder.</>;
+		if (!cleanMusicList) return <>Please, open a music folder.</>;
 
-		return fileSystem.music.map((musicFileHandler, index) =>
+		return cleanMusicList.map((musicFileHandler, index) =>
 			<MusicItem
 				key={index}
 				musicFileHandler={musicFileHandler}
