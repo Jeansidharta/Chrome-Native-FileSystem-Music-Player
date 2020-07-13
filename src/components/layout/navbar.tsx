9d8fa7c21bc79c4f861c80230cf29d4b5a02a946
jean sidharta <jeansidharta@gmail.com>
2020-07-13 15:19:52 -0300

    added searchbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useFileSystem } from '../../contexts/file-system';
+import { useSortFilter } from '../../contexts/sort-filter';
 
 const Root = styled.header`
-	height: 10vh;
 	display: flex;
+	flex-direction: column;
+	justify-content: center;
 	align-items: center;
 	padding: 8px 16px;
-	flex-direction: row-reverse;
 	box-shadow: -3px 3px 3px rgba(0, 0, 0, 0.3);
+`;
+
+const ButtonsContainer = styled.nav`
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	width: 100%;
+`;
+
+const SearchInput = styled.input.attrs(props => ({ type: 'text', ...props }))`
+	max-width: 200px;
+	width: 100%;
+	padding: 4px 8px;
+	border-width: 2px;
+	border-color: rgba(0, 0, 0, 0.3);
+	border-radius: 4px;
+	${(props: { isSearching: boolean }) => props.isSearching ? `
+		border-color: red;
+	` : `
+	`}
 `;
 
 const Button = styled.button`
@@ -17,10 +38,22 @@ const Button = styled.button`
 
 const Navbar = () => {
 	const { requestDirectoryAccess } = useFileSystem();
+	const { setSearchString, searchString } = useSortFilter();
+
+	function handleSearchChange (event: React.ChangeEvent<HTMLInputElement>) {
+		const value = event.target.value;
+		setSearchString(value);
+	}
 
 	return (
 		<Root>
-			<Button onClick={requestDirectoryAccess}>Select folder</Button>
+			<ButtonsContainer>
+				<Button onClick={requestDirectoryAccess}>Select folder</Button>
+			</ButtonsContainer>
+			<SearchInput
+				onChange={handleSearchChange}
+				isSearching={Boolean(searchString)}
+			/>
 		</Root>
 	);
 }
