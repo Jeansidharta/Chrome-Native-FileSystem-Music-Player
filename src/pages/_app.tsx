117fb47eec1a129fdbc2419902658bf8025f6086
jean sidharta <jeansidharta@gmail.com>
2020-07-24 03:54:23 -0300

    Organized app imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,24 @@
+// Framework
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 import Head from 'next/head';
-import Navbar from '../components/layout/navbar';
-import Providers from '../contexts';
-import MusicPlayer from '../services/music-player';
-import Footer from '../components/layout/footer';
+
+// Libs
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+// Services
+import MusicPlayer from '../services/music-player';
 import ModalRenderer from '../services/modal-renderer';
 import DocumentTitleUpdater from '../services/document-title-updater';
 import Shortcuts from '../services/shortcuts';
+import DragAndDrop from '../services/drag-and-drop';
+
+// Layout components
+import Navbar from '../components/layout/navbar';
+import Footer from '../components/layout/footer';
+
+// Misc
+import Providers from '../contexts';
 
 type MyAppProps = React.PropsWithoutRef<{
 	Component: any,
@@ -45,6 +55,7 @@ const MyApp: MyAppComponent = ({ Component, pageProps }) => {
 				<ModalRenderer />
 				<DocumentTitleUpdater />
 				<Shortcuts />
+				<DragAndDrop />
 			</Providers>
 		</>
 	);
